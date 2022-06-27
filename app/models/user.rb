6633e3da206d4ed9c6b908c32ab695f7109eb352
vincent <vincentuong@yahoo.com>
2022-06-26 22:59:41 -0500

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :appointments, dependent: :destroy
+    has_many :doctors, through: :appointments
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, confirmation: true, length: { in: 5..20 }
 end
