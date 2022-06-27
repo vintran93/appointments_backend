70c8ff541226c7b56654215778542910e0b37f73
vincent <vincentuong@yahoo.com>
2022-06-26 23:05:06 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :doctors
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :doctors, only: [:index, :show]
+      resources :users, only: [:create] do 
+        resources :appointments
+      end
+    end
+  end
+  
+  post 'login', to: 'authentication#login'
+  get 'auto_login', to: 'authentication#auto_login'
+  get 'user_is_authed', to: 'authentication#user_is_authed'
 end
