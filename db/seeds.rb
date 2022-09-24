d42ed78ec547dc59b575e8ecbf6a5ddf451172c1
vincent <vincentuong@yahoo.com>
2022-09-23 21:52:21 -0500

    first heroku commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Doctor.create([{
     education: 'Bachelor of Sciece (BS), Doctor of Medicine (MD)', 
     specialty: 'Primary Care Physician', 
     experience: '6 Years', 
-    image: 'https://ddcorlando.com/wp-content/uploads/2020/01/beautiful-young-female-doctor-looking-camera-office_1301-7781.jpg', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115353_doctor-images-png-transparent-female-doctor-png-png.png', 
     address: "1200 Oak Hill Road Suite 1, Austin TX 78548"
 },
 {
@@ -18,7 +18,7 @@ Doctor.create([{
     education: 'Bachelor of Science (BS), Doctor of Medicine (MD)', 
     specialty: 'Dermatology', 
     experience: '3 Years', 
-    image: 'https://s3.amazonaws.com/freestock-prod/450/freestock_65842171.jpg', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115495_sports-doctor-hd-png-download.png', 
     address: "1200 Oak Hill Road Suite 3, Austin TX 78548"
 },
 {
@@ -26,7 +26,7 @@ Doctor.create([{
     education: 'Doctor of Medicine (MD), Fellow American College of Cardiology (FACC)', 
     specialty: 'Cardiology', 
     experience: '11 Years', 
-    image: 'https://www.kindpng.com/picc/m/111-1115348_doctor-png-clipart-doctor-isolated-transparent-png.png', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115354_doctor-png-file-download-free-transparent-background-doctor.png', 
     address: "1200 Oak Hill Road Suite 8, Austin TX 78748"
 },
 {
@@ -34,15 +34,15 @@ Doctor.create([{
     education: 'Bachelor of Science (BS), Doctor of Physical Therapy (DPT)', 
     specialty: 'Physical Therapy', 
     experience: '12 Years', 
-    image: 'https://thumbs.dreamstime.com/b/african-american-black-doctor-man-over-blue-background-89746088.jpg', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115368_doctors-png-image-doctor-transparent-png-png-download.png', 
     address: "8000 Town Square Plaza Suite 3, Round Rock TX 78755"
 },
 {
-    name: 'Dr. Tran', 
+    name: 'Dr. Rodriguez', 
     education: 'Bachelor of Science (BS), Doctor of Occupational Therapy (DOT)', 
     specialty: 'Occupational Therapy', 
     experience: '7 Years', 
-    image: 'https://asianhealthservices.org/wp-content/uploads/2020/07/20200226_0049-731x1024.jpg', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115357_female-doctor-png-picture-female-doctor-png-transparent.png', 
     address: "8000 Town Square Plaza Suite 5, Round Rock TX 78755"
 },
 {
@@ -50,31 +50,31 @@ Doctor.create([{
     education: 'Master of Science (MS), Licensed Dietition (LDN)', 
     specialty: 'Dietition', 
     experience: '5 Years', 
-    image: 'https://media.gettyimages.com/photos/female-doctor-smiling-over-white-background-picture-id1053401358?s=612x612', 
+    image: 'https://png.pngitem.com/pimgs/s/111-1115738_personal-care-services-to-our-valued-patients-in.png', 
     address: "8000 Town Square Plaza Suite 11, Round Rock TX 78755"
 },
 {
-    name: 'Dr. Rodriguez', 
+    name: 'Dr. Rios', 
     education: 'Bachelor of Science (BS), Doctor of Medicine (MD)', 
     specialty: 'Family Physician', 
     experience: '10 Years', 
-    image: 'https://img.freepik.com/free-photo/pretty-latina-doctor-white-background_188913-1533.jpg?w=2000', 
+    image: 'https://png.pngitem.com/pimgs/s/490-4909562_image-of-rite-aid-pharmacist-doctor-png-transparent.png', 
     address: "2800 Town Plaza Blvd Suite 12 Pflugerville, TX 78543"
 },
 {
-    name: 'Dr. Hamilton', 
+    name: 'Dr. Anand', 
     education: 'Bachelor of Science (BS), Doctor of Osteopathic Medicine (DO)', 
     specialty: 'Neurology', 
     experience: '12 Years', 
-    image: 'https://media.istockphoto.com/photos/happy-healthcare-practitioner-picture-id138205019?k=20&m=138205019&s=612x612&w=0&h=KpsSMVsplkOqTnAJmOye4y6DcciVYIBe5dYDgYXLVW4=', 
+    image: 'https://png.pngitem.com/pimgs/s/527-5278112_dr-sumit-agrawal-indian-male-doctor-png-transparent.png', 
     address: "2800 Town Plaza Blvd Suite 4 Pflugerville, TX 78543"
 },
 {
-    name: 'Dr. Kim', 
+    name: 'Dr. Jones', 
     education: 'Bachelor of Arts (BA), Doctor of Osteopathic Medicine (DO)', 
     specialty: 'Radiology', 
     experience: '9 Years', 
-    image: 'https://www.vippskorea.com/img/about-vip/vip-doctor-01.jpg', 
+    image: 'https://png.pngitem.com/pimgs/s/455-4554771_doctor-png-female-doctor-transparent-background-png-download.png', 
     address: "2800 Town Plaza Blvd Suite 2 Pflugerville, TX 78543"
 },
 {
@@ -82,6 +82,6 @@ Doctor.create([{
     education: 'Bachelor of Science (BS), Doctor of Osteopathic Medicine (DO)',
     specialty: 'Emergency Medicine',
     experience: '5 Years',
-    image: 'https://t4.ftcdn.net/jpg/02/70/93/95/360_F_270939503_maJebkk8RKQzfQVB75WMwWGIHtIgxmvZ.jpg',
+    image: 'https://png.pngitem.com/pimgs/s/490-4900561_physician-hd-png-download.png',
     address: '2800 Town Plaza Blvd Suite 10 Pflugerville, TX 78543'
 }])
